8eb8080a1156555bfea5ecf8cbe1e5a8900e7948
Max Murdoch <max@maxmurdo.ch>
2017-02-19 15:29:53 +0000

    chore: use yarn instead of npm; upgrade webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,126 @@
+const { resolve, join } = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const postcssImport = require('postcss-import')
+const modules = require('postcss-modules')
+const autoreset = require('postcss-autoreset')
+const cssNext = require('postcss-cssnext')
+const values = require('postcss-modules-values')
+const fontMagician = require('postcss-font-magician')
+
+const postcssLoaders = [
+  {
+    loader: 'style-loader',
+  },
+  {
+    loader: 'css-loader',
+    options: {
+      modules: true,
+      localIdentName: '[name]__[local]__[hash:base64:5]',
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: [
+        modules,
+        postcssImport,
+        fontMagician({
+          hosted: resolve(__dirname, 'input', 'style', 'fonts'),
+        }),
+        autoreset({
+          reset: {
+            fontSize: '100%',
+            boxSizing: 'border-box',
+            margin: 0,
+            padding: 0,
+          },
+        }),
+        cssNext,
+        values,
+      ],
+    },
+  },
+]
+
+module.exports = () => ({
+  entry: {
+    main: resolve(__dirname, 'input', 'index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: join(__dirname, 'output'),
+  },
+  devServer: {
+    port: 4000,
+    host: 'localhost',
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'minimal',
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'stage-2'],
+            },
+          },
+          {
+            loader: 'eslint-loader',
+          },
+        ],
+        include: join(__dirname, 'input'),
+      },
+      {
+        test: /\.css$/,
+        use: postcssLoaders,
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+          {
+            loader: 'img-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['commons', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'input', 'index.html'),
+      inject: 'body',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true,
+      },
+      compress: {
+        screw_ie8: true,
+      },
+      comments: false,
+    }),
+  ],
+})
---
+++ webpack.config.prod.js
@@ -0,0 +1,115 @@
+const { resolve, join } = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const postcssImport = require('postcss-import')
+const modules = require('postcss-modules')
+const autoreset = require('postcss-autoreset')
+const cssNext = require('postcss-cssnext')
+const values = require('postcss-modules-values')
+const fontMagician = require('postcss-font-magician')
+
+const postcssLoaders = [
+  {
+    loader: 'style-loader',
+  },
+  {
+    loader: 'css-loader',
+    options: {
+      modules: true,
+      localIdentName: '[name]__[local]__[hash:base64:5]',
+    },
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: [
+        modules,
+        postcssImport,
+        fontMagician({
+          hosted: resolve(__dirname, 'input', 'style', 'fonts'),
+        }),
+        autoreset({
+          reset: {
+            fontSize: '100%',
+            boxSizing: 'border-box',
+            margin: 0,
+            padding: 0,
+          },
+        }),
+        cssNext,
+        values,
+      ],
+    },
+  },
+]
+
+module.exports = (    ) => ({
+  entry: {
+    main: resolve(__dirname, 'input', 'index.js'),
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: join(__dirname, 'output'),
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'stage-2'],
+            },
+          },
+          {
+            loader: 'eslint-loader',
+          },
+        ],
+        include: join(__dirname, 'input'),
+      },
+      {
+        test: /\.css$/,
+        use: postcssLoaders,
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+          {
+            loader: 'img-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['commons', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'input', 'index.html'),
+      inject: 'body',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true,
+      },
+      compress: {
+        screw_ie8: true,
+      },
+      comments: false,
+    }),
+  ],
+})
