6faabc09a51b5f7c5e30f9796b8c074df8e62532
Max Murdoch <max@maxmurdo.ch>
2017-08-05 14:06:12 +0100

    Remove unused div nesting

--- input/components/header/index.js
+++ input/components/header/index.js
@@ -10,48 +10,46 @@ import {small} from '../../constants/media'
 const HEADER_HEIGHT = '10vh'
 
 const Header = () => {
-  return div([
-    div(
-      {
-        'data-aos': 'fade-in',
-        'data-aos-delay': 800,
-        className: css({
-          zIndex: 6,
-          height: HEADER_HEIGHT,
-          display: 'flex',
-          width: '100%',
-          position: 'fixed',
-          alignItems: 'stretch',
-          [small]: {bottom: 0}
-        })
-      },
-      [
-        container([
-          grid({justify: 'between', align: 'center'}, [
-            cell(
-              {
-                small: {column: 1, of: 2},
-                medium: {column: 1, of: 2},
-                large: {column: 1, of: 2},
-                align: 'center'
-              },
-              [logo()]
-            ),
-            cell(
-              {
-                small: {column: 1, of: 2},
-                medium: {column: 1, of: 2},
-                large: {column: 1, of: 2},
-                align: 'center',
-                justify: 'end'
-              },
-              [menuIcon()]
-            )
-          ])
+  return div(
+    {
+      'data-aos': 'fade-in',
+      'data-aos-delay': 800,
+      className: css({
+        zIndex: 10,
+        height: HEADER_HEIGHT,
+        display: 'flex',
+        width: '100%',
+        position: 'fixed',
+        alignItems: 'stretch',
+        [small]: {bottom: 0}
+      })
+    },
+    [
+      container([
+        grid({justify: 'between', align: 'center'}, [
+          cell(
+            {
+              small: {column: 1, of: 2},
+              medium: {column: 1, of: 2},
+              large: {column: 1, of: 2},
+              align: 'center'
+            },
+            [logo()]
+          ),
+          cell(
+            {
+              small: {column: 1, of: 2},
+              medium: {column: 1, of: 2},
+              large: {column: 1, of: 2},
+              align: 'center',
+              justify: 'end'
+            },
+            [menuIcon()]
+          )
         ])
-      ]
-    )
-  ])
+      ])
+    ]
+  )
 }
 
 export default hh(Header)
