1a1a1f54e2fc746d3eb1efd71532575a82b42de0
Max Murdoch <max@maxmurdo.ch>
2017-08-05 14:02:49 +0100

    Update cursor styles

--- input/components/link/index.js
+++ input/components/link/index.js
@@ -1,25 +1,39 @@
+import R from 'ramda'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
 import {hh, h} from 'react-hyperscript-helpers'
 import {css} from 'glamor'
+import internalCursor from '../../assets/cursor-2-internal.svg'
+import externalCursor from '../../assets/cursor-2-external.svg'
+import defaultCursor from '../../assets/cursor-2.svg'
+
+const cursorFor = R.cond([
+  [R.equals('internal'), R.always(`url(${internalCursor}) 8 8, auto`)],
+  [R.equals('external'), R.always(`url(${externalCursor}), auto`)],
+  [R.T, R.always(`url(${defaultCursor}), auto`)]
+])
 
 const LinkWrapper = ({
   color = 'inherit',
   to,
   className,
   onMouseEnter,
+  target,
   onMouseLeave,
   children,
   ...props
 }) => {
   const linkStyle = css(
     {
-      transition: '250ms opacity',
+      transition: 'opacity 250ms cubic-bezier(0.2, 1, 0.4, 1)',
       display: 'inline-flex',
       flex: '0 1 auto',
       textDecoration: 'none',
       color,
-      ':hover': {opacity: 0.6},
+      ':hover': {
+        opacity: 0.6,
+        cursor: cursorFor(target)
+      },
       ':focus': {backgroundColor: 'blue', outline: 0},
       ':visited': {color}
     },
--- input/constants/link-style/index.js
+++ input/constants/link-style/index.js
@@ -1,11 +1,8 @@
-import cursorActive from '../../assets/cursor-active.svg'
+import defaultCursor from '../../assets/cursor-2.svg'
 
 const linkStyle = {
-  cursor: `url(${cursorActive}), auto`,
-  transition: '300ms all cubic-bezier(0.785, 0.135, 0.15, 0.86)',
-  ':hover': {
-    cursor: `url(${cursorActive}), auto`
-  }
+  cursor: `url(${defaultCursor}), auto`,
+  transition: '300ms all cubic-bezier(0.785, 0.135, 0.15, 0.86)'
 }
 
 export default linkStyle
