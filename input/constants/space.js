031a68a1633b936b6d6a4fa7d9f551b6b75ca400
Max Murdoch <max@maxmurdo.ch>
2018-01-23 16:25:16 +0000

    vw -> rem

--- input/constants/space.js
+++ input/constants/space.js
@@ -1,7 +1,7 @@
 import R from 'ramda'
 
 const numScale = R.scan(a => R.multiply(a, 1.5), 1, R.range(1, 9))
-const appendUnit = R.partialRight(R.concat, ['vw'])
+const appendUnit = R.partialRight(R.concat, ['rem'])
 const space = R.map(appendUnit, R.map(R.toString, numScale))
 
 export default space
