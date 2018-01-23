a59d7d1b358c92e8409cce9e465f8d02e0a785f9
Max Murdoch <max@maxmurdo.ch>
2018-01-23 16:25:43 +0000

    Update menu/header

--- input/components/header/index.js
+++ input/components/header/index.js
@@ -1,31 +1,37 @@
 import {div, hh} from 'react-hyperscript-helpers'
 import {css} from 'glamor'
-import container from '../site-container'
+import containerBox from '../site-container-box'
 import grid from '../grid'
 import logo from '../logo'
 import menuIcon from '../../containers/menu-icon'
 import cell from '../cell'
-import {small} from '../../constants/media'
+import {small, medium, large} from '../../constants/media'
+import {smallDouble, mediumDouble, largeDouble} from '../../constants/gutter'
 
 const HEADER_HEIGHT = '10vh'
+const SMALL_HEADER_HEIGHT = smallDouble
+const MEDIUM_HEADER_HEIGHT = mediumDouble
+const LARGE_HEADER_HEIGHT = largeDouble
 
 const Header = () => {
   return div(
     {
       'data-aos': 'fade-in',
-      'data-aos-delay': 800,
+      'data-aos-delay': 1200,
       className: css({
         zIndex: 2,
-        height: HEADER_HEIGHT,
         display: 'flex',
         width: '100%',
         position: 'fixed',
         alignItems: 'stretch',
-        [small]: {bottom: 0}
+        height: SMALL_HEADER_HEIGHT,
+        [small]: {bottom: 0},
+        [medium]: {height: MEDIUM_HEADER_HEIGHT},
+        [large]: {height: LARGE_HEADER_HEIGHT}
       })
     },
     [
-      container([
+      containerBox([
         grid({justify: 'between', align: 'center'}, [
           cell(
             {
@@ -52,6 +58,11 @@ const Header = () => {
   )
 }
 
-export default hh(Header)
+export {
+  HEADER_HEIGHT,
+  SMALL_HEADER_HEIGHT,
+  MEDIUM_HEADER_HEIGHT,
+  LARGE_HEADER_HEIGHT
+}
 
-export {HEADER_HEIGHT}
+export default hh(Header)
--- input/screens/menu/index.js
+++ input/screens/menu/index.js
@@ -2,13 +2,14 @@ import R from 'ramda'
 import {css} from 'glamor'
 import {div} from 'react-hyperscript-helpers'
 
-import container from '../../components/site-container'
+import bodyText from '../../components/body-text'
 import cell from '../../components/cell'
+import containerBox from '../../components/site-container-box'
 import crossLink from '../../components/cross-link'
 import grid from '../../components/grid'
 import header from '../../components/header'
-import bodyText from '../../components/body-text'
-import {small, medium, large} from '../../constants/media'
+import link from '../../components/link'
+import {medium, large} from '../../constants/media'
 
 import space from '../../constants/space'
 import {HEADER_HEIGHT} from '../../components/header'
@@ -18,6 +19,7 @@ const Menu = ({closeMenu, isOpen}) => {
     {
       className: css({
         position: 'fixed',
+        width: '100%',
         top: 0,
         bottom: isOpen ? 0 : '100%',
         right: 0,
@@ -31,7 +33,9 @@ const Menu = ({closeMenu, isOpen}) => {
       div(
         {
           className: css({
-            minHeight: '100vh',
+            position: 'fixed',
+            height: '90vh',
+            width: '100%',
             transform: isOpen ? 'translateY(0)' : 'translateY(-150%)',
             transitionDelay: isOpen ? 0 : '100ms',
             [medium]: {marginTop: HEADER_HEIGHT},
@@ -39,72 +43,75 @@ const Menu = ({closeMenu, isOpen}) => {
           })
         },
         [
-          crossLink(
-            {
-              to: '/',
-              onClick: closeMenu
-            },
-            [
-              container([
-                grid({justify: 'between', align: 'center'}, [
-                  cell([
-                    div(
-                      {
-                        className: css({
-                          [small]: {
-                            marginTop: R.nth(2, space),
-                            marginBottom: HEADER_HEIGHT
+          grid({align: 'stretch'}, [
+            cell([
+              crossLink(
+                {
+                  to: '/',
+                  onClick: closeMenu,
+                  style: {
+                    height: '100%',
+                    width: '100%',
+                    display: 'flex',
+                    alignItems: 'stretch'
+                  }
+                },
+                [
+                  containerBox([
+                    grid({justify: 'between', align: 'flex-end'}, [
+                      cell({align: 'end'}, [
+                        div(
+                          {
+                            className: css({
+                              display: 'flex',
+                              flexDirection: 'column',
+                              justifyContent: 'flex-end'
+                            })
                           },
-                          [medium]: {marginBottom: R.nth(2, space)},
-                          [large]: {marginBottom: R.nth(2, space)}
-                        })
-                      },
-                      R.addIndex(R.map)(
-                        (text, index) => {
-                          return bodyText(
-                            {
-                              smallSize: 3,
-                              mediumSize: 4,
-                              size: 5,
-                              className: css({
-                                willChange: 'transform, opacity',
-                                opacity: isOpen ? 1 : 0,
-                                transform: isOpen
-                                  ? 'translateY(0)'
-                                  : 'translateY(-20px)',
-                                transitionDelay: isOpen
-                                  ? `${R.add(R.multiply(index, 50), 50)}ms`
-                                  : '0',
-                                transitionProperty: 'opacity, transform',
-                                transitionDuration: '200ms',
-                                transitionTimingFunction:
-                                  'cubic-bezier(0.2, 0.7, 0.2, 0.8)'
-                              })
-                            },
-                            [text]
+                          R.addIndex(R.map)(
+                            ({to, text}, index) =>
+                              link({to}, [
+                                bodyText(
+                                  {
+                                    smallSize: 3,
+                                    mediumSize: 4,
+                                    size: 5,
+                                    className: css({
+                                      marginTop: R.nth(0, space),
+                                      willChange: 'transform, opacity',
+                                      opacity: isOpen ? 1 : 0,
+                                      transform: isOpen
+                                        ? 'translateY(0)'
+                                        : 'translateY(-20px)',
+                                      transitionDelay: isOpen
+                                        ? `${R.add(
+                                            R.multiply(index, 50),
+                                            50
+                                          )}ms`
+                                        : '0',
+                                      transitionProperty: 'opacity, transform',
+                                      transitionDuration: '200ms',
+                                      transitionTimingFunction:
+                                        'cubic-bezier(0.2, 0.7, 0.2, 0.8)'
+                                    })
+                                  },
+                                  [text]
+                                )
+                              ]),
+                            [
+                              {text: 'About', to: '/about'},
+                              {text: 'Work', to: '/work'},
+                              {text: 'Contact', to: '/contact'}
+                            ]
                           )
-                        },
-                        [
-                          'Hi, I’m Max. This is my tiny design and programming studio.',
-                          'I tend to take responsibility for the design and build of project from beginning to end and tend not to do incremental optimisation work.',
-                          'I’m booked till September, but if you would like to talk about working together, please get in touch.',
-                          'Hi, I’m Max. This is my tiny design and programming studio.',
-                          'I tend to take responsibility for the design and build of project from beginning to end and tend not to do incremental optimisation work.',
-                          'I’m booked till September, but if you would like to talk about working together, please get in touch.',
-                          'I’m booked till September, but if you would like to talk about working together, please get in touch.',
-                          'Hi, I’m Max. This is my tiny design and programming studio.',
-                          'I tend to take responsibility for the design and build of project from beginning to end and tend not to do incremental optimisation work.',
-                          'I’m booked till September, but if you would like to talk about working together, please get in touch.',
-                          'Hi, I’m Max. This is my tiny design and programming studio.',
-                          'I tend to take responsibility for the design and build of project from beginning to end and tend not to do incremental optimisation work.'
-                        ]
-                      )
-                    )
+                        )
+                      ])
+                    ])
                   ])
-                ])
-              ])
-            ]
-          )
+                ]
+              )
+            ])
+          ])
         ]
       )
     ]
