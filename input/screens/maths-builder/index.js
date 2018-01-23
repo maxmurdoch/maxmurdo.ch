fed12359c0510d223f1c8cd60f4f8213e9fa782b
Max Murdoch <max@maxmurdo.ch>
2018-01-23 16:28:27 +0000

    Add MathsBuilder screen

--- input/screens/maths-builder/index.js
+++ input/screens/maths-builder/index.js
@@ -1,7 +1,108 @@
-import {h, p} from 'react-hyperscript-helpers'
+import {css} from 'glamor'
+import {div, img} from 'react-hyperscript-helpers'
+import text from '../../components/text'
+import menu from '../../containers/menu'
+import grid from '../../components/grid'
+import cell from '../../components/cell'
+import menuBlur from '../../containers/menu-blur'
+import fullPageHeader from '../../components/full-page-header'
+import container from '../../components/site-container'
+import containerBox from '../../components/site-container-box'
+import mathsBuildersLogo from '../../assets/maths-builders-logo.svg'
 
-const MathsBuilder = ({}) => {
-  return p('Hi')
+const MathsBuilder = () => {
+  return div([
+    menu(),
+    menuBlur([
+      fullPageHeader([
+        container({bottom: true}, [
+          div(
+            {
+              className: css({
+                display: 'flex',
+                flexWrap: 'wrap',
+                height: '100%',
+                border: '2px solid black'
+              })
+            },
+            [
+              div(
+                {
+                  className: css({
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '50%',
+                    height: 'calc(100% / 3)',
+                    borderRight: '2px solid black'
+                  })
+                },
+                [
+                  container([
+                    div(
+                      {
+                        className: css({
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center'
+                        })
+                      },
+                      [
+                        img({
+                          src: mathsBuildersLogo
+                        })
+                      ]
+                    )
+                  ])
+                ]
+              ),
+              div(
+                {
+                  className: css({
+                    width: '50%',
+                    height: 'calc(100% / 3)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  })
+                },
+                [
+                  container([
+                    div(
+                      {
+                        className: css({
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center'
+                        })
+                      },
+                      [text({size: 4}, 'Maths builders')]
+                    )
+                  ])
+                ]
+              ),
+              div(
+                {
+                  className: css({
+                    width: '100%',
+                    height: 'calc(100% / 3 * 2)',
+                    borderTop: '2px solid black'
+                  })
+                },
+                [
+                  containerBox([
+                    text({size: 3}, [
+                      'Maths builders is a service that helps you study for a GCSE maths exam.'
+                    ])
+                  ])
+                ]
+              )
+            ]
+          )
+        ])
+      ])
+    ])
+  ])
 }
 
 export default MathsBuilder
