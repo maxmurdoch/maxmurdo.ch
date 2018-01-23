f0da8da9eb3548e926e46df5a2a39940c6d97bd0
Max Murdoch <max@maxmurdo.ch>
2018-01-23 16:26:45 +0000

    Add FullPageHeader component

---
+++ input/components/full-page-header/index.js
@@ -0,0 +1,31 @@
+import {hh, div} from 'react-hyperscript-helpers'
+import {css} from 'glamor'
+
+import {
+  SMALL_HEADER_HEIGHT,
+  MEDIUM_HEADER_HEIGHT,
+  LARGE_HEADER_HEIGHT
+} from '../header'
+import {medium, large} from '../../constants/media'
+
+const FullPageHeader = ({children, className, ...props}) => {
+  return div(
+    {
+      className: css(
+        {
+          width: '100%',
+          height: '100vh',
+          display: 'flex',
+          paddingTop: SMALL_HEADER_HEIGHT,
+          [medium]: {paddingTop: MEDIUM_HEADER_HEIGHT},
+          [large]: {paddingTop: LARGE_HEADER_HEIGHT}
+        },
+        className
+      ),
+      ...props
+    },
+    [children]
+  )
+}
+
+export default hh(FullPageHeader)
